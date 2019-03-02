46ecd82068d478c84eca6651a35646f61b5a3d9c
isalevine <isaacdlevine@gmail.com>
2019-03-01 16:19:15 -0800

    Done.

---
+++ db/migrate/01_create_artists.rb
@@ -0,0 +1,16 @@
+class CreateArtists < ActiveRecord::Migration
+  def up
+  end
+
+  def down
+  end
+
+  def change
+    create_table :artists do |t|
+      t.string :name
+      t.string :genre
+      t.string :age
+      t.string :hometown
+    end
+  end
+end
